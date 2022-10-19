75f35f40737a2d5bab144c40cf362c2d048fcfb4
removedporn <86824510+removedporn@users.noreply.github.com>
2022-10-19 08:34:17 +0800

    Delete exercises directory

--- exercises/clippy/target/debug/deps/clippy3-55a9261f82d68803.d
+++
@@ -1,7 +0,0 @@
-C:\Windows\System32\rustlings\exercises\clippy\target\debug\deps\clippy3-55a9261f82d68803.rmeta: clippy3.rs
-
-C:\Windows\System32\rustlings\exercises\clippy\target\debug\deps\clippy3-55a9261f82d68803.d: clippy3.rs
-
-clippy3.rs:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
